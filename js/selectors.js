2a50d2c62876bd94003cb12ae15c9413bc25ceb1
Mark Kauffman <markkauffman22@gmail.com>
2019-10-03 14:58:38 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -3,3 +3,26 @@
 function paragraphSelector() {
   
 }
+
+///////////////////////////////
+	'use-strict';
+
+
+// declare your functions here... 
+function paragraphSelector () {
+  return $('p');
+}
+
+function lastImageSelector () {
+  return $('img:last');
+}
+
+function ninjaBabySelector () {
+  return $('#baby-ninja');
+}
+function divSelector () {
+  return $('.pics');
+}
+function firstListItem (argument) {
+ return $('#pic-list li:first');
+}
